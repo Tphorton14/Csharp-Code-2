331919d9169b43c9147d999015247e3c7fd6673e
Tphorton <Tphorton14@gmail.com>
2020-04-12 16:26:34 -0400

    update practice code

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -11,16 +11,22 @@ namespace GradeBook
         // when dotnet runs it looks for "Main"
         static void Main(string[] args)
         {
-            var book = new Book();
+            var book = new Book("My Grade Book");
             book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            
 
             var grades = new List<double>() { 12.7, 14.5, 15.8 };
             grades.Add(56.1);
 
             // how to loop through array and sum all numbers in array
             var result = 0.0;
+            var highGrade = double.MinValue;
             // looping through collection of numbers
             foreach(var number in grades) {
+                
+                highGrade = Math.Max(number, highGrade);
                 // adding numbers to result
                result += number;
             }
