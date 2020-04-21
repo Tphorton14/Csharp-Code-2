2c8426d3ca2a6825eb207ae10fb9dd5c37dfb682
Tphorton <Tphorton14@gmail.com>
2020-04-21 10:08:53 -0400

    refactor code

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook {
@@ -12,8 +13,30 @@ class Book {
     public void AddGrade(double grade) {
         grades.Add(grade);
     }
+    public void ShowStats()
+        {
+             // how to loop through array and sum all numbers in array
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            // looping through collection of numbers
+            foreach(var number in grades) {
+                lowGrade = Math.Min(number, lowGrade);
+                highGrade = Math.Max(number, highGrade);
+                // adding numbers to result
+               result += number;
+            }
+            // dividing by Count
+            result /= grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The highest grade is {highGrade}");
+            Console.WriteLine($"The average grade is {result:N1}!");
+            
+        }
     List<double> grades;
     private string name;
+
+        
     }
 
 }
