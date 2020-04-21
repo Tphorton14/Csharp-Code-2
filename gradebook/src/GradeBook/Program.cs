202bc189268c6876dee0da3be18ff260f613f8e2
Tphorton <Tphorton14@gmail.com>
2020-04-21 10:03:25 -0400

    grade outputs are working

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -15,23 +15,27 @@ namespace GradeBook
             book.AddGrade(89.1);
             book.AddGrade(90.5);
             book.AddGrade(77.5);
+            book.ShowStats();
             
 
-            var grades = new List<double>() { 12.7, 14.5, 15.8 };
+            var grades = new List<double>() { 12.7, 14.5, 15.8 , 4.1};
             grades.Add(56.1);
 
             // how to loop through array and sum all numbers in array
             var result = 0.0;
             var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
             // looping through collection of numbers
             foreach(var number in grades) {
-                
+                lowGrade = Math.Min(number, lowGrade);
                 highGrade = Math.Max(number, highGrade);
                 // adding numbers to result
                result += number;
             }
             // dividing by Count
             result /= grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The highest grade is {highGrade}");
             Console.WriteLine($"The average grade is {result:N1}!");
         }
 
